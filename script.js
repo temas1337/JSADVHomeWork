cb6fdefd7c7b907aeaddcb40dbece04aae0f225d
temas1337 <105651411+temas1337@users.noreply.github.com>
2022-09-24 18:32:27 +0700

    DZ

--- script.js
+++ script.js
@@ -1,19 +1,71 @@
-const goods = [
-    { title: 'Shirt', price: 150 },
-    { title: 'Socks', price: 50 },
-    { title: 'Jacket', price: 350 },
-    { title: 'Shoes', price: 250 },
-    ];
+class card {
+    addGoods () {
+    }
+    totalCost() {
 
-const renderGoods = (title, price) => {
+    }
+    payment() {
+
+    }
+
+    deletethink() {
+
+    }
+};
+
+class cardThink{
+
+}
+class GoodsItem {
+    constructor(title, price) {
+    this.title = title;
+    this.price = price;
+ }
+   render() {
     return `<div class="goods-item">
     <img class="img_card" src="tshirt-2.jpg">
-    <h3 class="card_title">${title}</h3>
-    <p class="card_price">${price}</p>
-    <button class="cart-button" type="button">Корзина</button></div> `;
+    <h3 class="card_title">${this.title}</h3>
+    <p class="card_price">${this.price}</p>
+    <button class="cart-button" type="button">Купить</button></div> `;
 };
-const renderGoodsList = (list) => {
-    let goodsList = list.map(item => renderGoods(item.title, item.price));
-document.querySelector('.goods-list').innerHTML = goodsList.join(''); // по умолчанию элементы после эл массивов ставится , join может изменить знак, поменяв его на пустоту можно избавится от запятой 
+    
 }
-renderGoodsList(goods);
+
+class goodsList {
+    constructor() {
+        this.goods = [];
+    }
+    fetchGoods() {
+        this.goods = [
+            { title: 'Shirt', price: 150 },
+            { title: 'Socks', price: 50 },
+            { title: 'Jacket', price: 350 },
+            { title: 'Shoes', price: 250 },
+            ];
+    }
+    render() {
+        let listHTML = '';
+        this.goods.forEach(good => {
+            const goodItem = new GoodsItem(good.title, good.price);
+            listHTML += goodItem.render();
+        });
+        document.querySelector('.goods-list').innerHTML = listHTML;
+    }
+
+    sum (){
+        let sumGoods = 0;
+        this.goods.forEach(good=> {
+            sumGoods += good.price;
+
+        });
+        console.log(sumGoods);
+    }
+}
+
+const list = new goodsList();
+list.fetchGoods();
+list.render();
+list.sum()
+
+
+
